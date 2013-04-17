7b68c017474566b8ad10f9b05e20da9bf6a628c7
Brent Edwards <brent.l.edwards@gmail.com>
2013-04-17 17:49:04 -0500

    Adding a helper class for local storage.

---
+++ Charmed.Win8/Storage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Charmed
+{
+	public sealed class Storage : IStorage
+	{
+		private readonly ISerializer serializer;
+
+		public Storage(ISerializer serializer)
+		{
+			this.serializer = serializer;
+		}
+
+		public async Task<T> Load<T>(string fileName)
+		{
+			var file = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+
+			var serializedData = await FileIO.ReadTextAsync(file, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+
+			T data = default(T);
+			if (!string.IsNullOrWhiteSpace(serializedData))
+			{
+				data = this.serializer.Deserialize<T>(serializedData);
+			}
+
+			return data;
+		}
+
+		public async Task Save(string fileName, object data)
+		{
+			var file = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+
+			var serializedData = this.serializer.Serialize(data);
+
+			await FileIO.WriteTextAsync(file, serializedData, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+		}
+	}
+}
