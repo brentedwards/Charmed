aa0959359326af3e3fef734cf0fce93480357d5f
Brent Edwards <brent.l.edwards@gmail.com>
2014-10-01 00:18:27 -0500

    Wiring up navigation by selecting a feed item in WP.

--- Charmed.Universal/Charmed.Universal.Shared/Helpers/ListViewHelper.cs
+++ Charmed.Universal/Charmed.Universal.Shared/Helpers/ListViewHelper.cs
@@ -41,6 +41,29 @@ namespace Charmed.Helpers
 					method.Invoke(listView.DataContext, null);
 				}
 			};
+
+			listView.SelectionChanged += (sender, args) =>
+			{
+				var methodName = GetItemClickMethodName(listView);
+
+				var dataContext = GetMethodContext(listView);
+				if (dataContext == null)
+				{
+					dataContext = listView.DataContext;
+				}
+
+				var method = dataContext.GetType().GetTypeInfo().GetDeclaredMethod(methodName);
+				var parms = method.GetParameters();
+
+				if (parms != null && parms.Length == 1)
+				{
+					method.Invoke(dataContext, new[] { args.AddedItems[0] });
+				}
+				else
+				{
+					method.Invoke(dataContext, null);
+				}
+			};
 		}
 
 		public static void SetItemClickMethodName(
@@ -53,6 +76,32 @@ namespace Charmed.Helpers
 		public static string GetItemClickMethodName(DependencyObject attached)
 		{
 			return (string)attached.GetValue(ItemClickMethodNameProperty);
+		}
+
+		public static readonly DependencyProperty MethodContextProperty =
+			DependencyProperty.RegisterAttached(
+				"MethodContext",
+				typeof(object),
+				typeof(ListViewHelper),
+				new PropertyMetadata(null, new PropertyChangedCallback((sender, args) =>
+				{
+					var listView = sender as ListViewBase;
+					if (listView != null)
+					{
+						if (!(listView.Tag is ListViewHelper))
+						{
+							listView.Tag = new ListViewHelper(listView);
+						}
+					}
+				})));
+
+		public static void SetMethodContext(ListViewBase element, object value)
+		{
+			element.SetValue(MethodContextProperty, value);
+		}
+		public static object GetMethodContext(ListViewBase element)
+		{
+			return element.GetValue(MethodContextProperty);
 		}
 	}
 }
